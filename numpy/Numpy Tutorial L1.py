f4e4fc5668df60ffa631cc6391fe2517ac81325a
clastro <37033795+clastro@users.noreply.github.com>
2022-01-21 00:28:21 +0900

    Create Numpy Tutorial L1.py

---
+++ numpy/Numpy Tutorial L1.py
@@ -0,0 +1,32 @@
+# Difficulty Level: L1 
+
+# Q. Import numpy as `np` and print the version number.
+
+import numpy as np
+print(np.__version__)
+
+# Q. Create a 1D array of numbers from 0 to 9 Desired output
+#> array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+np.array(range(0,10)) #내가 쓴 답
+np.arange(10)
+
+# Q. Create a 3×3 numpy array of all True’s
+
+np.array([[True,True,True], [True, True,True], [True, True,True]], np.bool) # 내가 쓴 답
+
+np.full((3,3), 1, dtype=bool)
+np.ones((3,3), dtype=bool)
+
+# Q. Extract all odd numbers from arr Input
+#> array([1, 3, 5, 7, 9])
+
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+arr[(arr % 2 == 1)] # 내가 쓴 답
+arr[arr % 2 == 1]
+
+
+# Q. Replace all odd numbers in arr with -1 Input:
+
+arr[arr % 2 == 1] = -1
