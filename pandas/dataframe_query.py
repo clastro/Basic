faaaec6e35cee9f27b70a2461e5b69fd61b863da
clastro <37033795+clastro@users.noreply.github.com>
2022-01-27 17:04:56 +0900

    Create dataframe_query.py

---
+++ pandas/dataframe_query.py
@@ -0,0 +1,7 @@
+#데이터프레임 내 multi columns에 대한 isin 함수가 필요할 때 (query를 사용)
+
+query = '(main_cate1 in @train_category) & (sub_cate1_1 in @train_category2) & (sub_cate2_1 in @train_category2) &' +\
+'(main_cate2 in @train_category2) & (sub_cate1_2 in @train_category2) & (sub_cate2_2 in @train_category2) &' +\
+'(main_cate3 in @train_category2) & (sub_cate1_3 in @train_category2) & (sub_cate2_3 in @train_category2)'   
+
+df.query(query)
