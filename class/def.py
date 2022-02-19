8c8fff6e1e29e365573e03d4a55b151c1757d34d
clastro <37033795+clastro@users.noreply.github.com>
2022-02-20 01:11:21 +0900

    Create def.py

---
+++ class/def.py
@@ -0,0 +1,11 @@
+# 1. funcName이라는 함수를 선언
+
+''' 
+x라는 인수는 int 값을 의미
+y는 float값을 입력받고 초기값은 1.5
+-> int 출력은 int로 하라는 의미
+x+y를 return하는 함수
+'''
+
+def funcName(x: int, y: float = 1.5) -> int:
+    return x + y
