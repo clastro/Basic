25914c60640bd206c32c6db8320ba6662a601f45
clastro <37033795+clastro@users.noreply.github.com>
2022-05-30 23:20:33 +0900

    Update itertools.py

--- function/itertools.py
+++ function/itertools.py
@@ -10,18 +10,18 @@ for i in itertools.cycle(cyclic_list):
     if counter>10:
         break
         
-#product
+#product 외적
 
 x = [1, 2, 3]
 y = ['A', 'B']
 print(list(itertools.product(x, y)))
 
-#permutations
+#permutations 순열
 
 x = [1, 2, 3]
 print(list(itertools.permutations(x)))
 
-#combinations
+#combinations 조합
 
 y = ['A', 'B', 'C', 'D']
 print(list(itertools.combinations(y, 3)))
@@ -31,7 +31,7 @@ print(list(itertools.combinations(y, 3)))
 z = ['A', 'B', 'C']
 print(list(itertools.combinations_with_replacement(z, 2)))
 
-#starmap
+#starmap 내부 연산
 
 x1 = [2, 3, 4, -6]
 x2 = [4, 3, 2, 1] 
