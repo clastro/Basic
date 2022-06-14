eeb011ee529dca35e0d17b6120c432cb71410b06
Song S.H <37033795+clastro@users.noreply.github.com>
2022-06-15 01:33:55 +0900

    Update __doc__.py

--- Built-in/__doc__.py
+++ Built-in/__doc__.py
@@ -2,6 +2,14 @@ def factorial(n):
     """returns n!"""
     return 1 if n < 2 else n * factorial(n-1)
 
-factorial.__doc__
+print(factorial.__doc__)
 
 # "returns n!" 이 출력됨
+
+def square_root(x):
+    """Returns the square root of the argument x"""
+    return x ** 0.5 
+
+print(square_root.__doc__)
+
+# message가 출력
