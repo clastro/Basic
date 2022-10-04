f57b43d7115b4e332b9941c91b93bb15e0e49de3
Song S.H <37033795+clastro@users.noreply.github.com>
2022-10-04 23:19:13 +0900

    Update dataframe_query.py

--- pandas/dataframe_query.py
+++ pandas/dataframe_query.py
@@ -5,3 +5,7 @@ query = '(main_cate1 in @train_category) & (sub_cate1_1 in @train_category2) & (
 '(main_cate3 in @train_category2) & (sub_cate1_3 in @train_category2) & (sub_cate2_3 in @train_category2)'   
 
 df.query(query)
+
+#DF1 rows - DF2 rows
+
+DF=DF1[~DF1.isin(DF2)].dropna(how = 'all')
