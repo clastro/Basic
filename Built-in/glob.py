5f2f796f40814171704f8ca894cbfb1b46e2aef1
Song S.H <37033795+clastro@users.noreply.github.com>
2022-12-20 19:08:55 +0900

    Update glob.py

--- Built-in/glob.py
+++ Built-in/glob.py
@@ -9,3 +9,5 @@ csv_files = glob.glob('/folder/sub_folder/*.csv')
 # csv 파일을 하위 폴더에서 모두 찾고 싶을 때
 
 csv_files = glob.glob('/folder/**/*.csv', recursive=True)
+
+glob.glob('**/*.csv') # ‘.csv’로 끝나는 모든 파일이 바로 아래 하위 디렉토리에서만 일치.
