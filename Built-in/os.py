2529eab0994fd6a0b2dd6c64f22c58d3e26d7c3b
Song S.H <37033795+clastro@users.noreply.github.com>
2023-01-18 21:57:24 +0900

    Update os.py

--- Built-in/os.py
+++ Built-in/os.py
@@ -4,3 +4,6 @@ os.system("slackbot -d '@id' -m 'complete AFib with %f minuates '"%elapsed_time)
 # Change Current Directory
 os.chdir("/smc_work/datanvme/smc/origin/")
 
+# path for import package
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.environ["PYTHONPATH"] = parent_dir + ":" + os.environ.get("PYTHONPATH", "")
