87f35958254438764f4af879d35028ef9020c1ee
Song S.H <37033795+clastro@users.noreply.github.com>
2023-07-28 11:52:34 +0900

    Update np_nditer.py

--- numpy/np_nditer.py
+++ numpy/np_nditer.py
@@ -5,3 +5,17 @@ a=np.array([[2,4,5],[3,6,7],[4,8,9]])
 for x in np.nditer(a, flags = ['external_loop'],order='F'): #Transpose array
   print(x)
 
+# [2,3,4]
+  [4,6,8]
+  [5,7,9]
+
+arr = np.array([1, 2, 3, 4, 5])  
+
+it = np.nditer(arr, flags=['c_index'])
+
+while not it.finished:
+    idx = it.index
+    print(arr[idx], end=' ')
+    it.iternext()
+    
+# 1 2 3 4 5
