0e07f4199200b4d61ba17ae7195e54d4b80d3954
Song S.H <37033795+clastro@users.noreply.github.com>
2023-08-29 00:57:24 +0900

    Create magic_method.py

---
+++ class/magic_method.py
@@ -0,0 +1,25 @@
+class Vector(object):
+
+    def __init__(self, *arg):
+        '''
+        Create a vector, example : v = Vector(5, 10)
+        '''
+
+        if len(arg) == 0:
+            self._x, self._y = 0, 0
+        else:
+            self._x, self._y = arg
+
+    def __repr__(self):
+        ''' Return the vector informations.'''
+        return 'Vector(%r, %r)' % (self._x, self._y)
+
+    def __add__(self, other):
+        ''' Return the vector add of self and other '''
+        return Vector(self._x + other._x, self._y + other._y)
+
+    def __mul__(self, other):
+        return Vector(self._x * other._x, self._y * other._y)
+
+    def __bool__(self):
+        return bool(max(self._x, self._y))
